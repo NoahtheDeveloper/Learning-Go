3f0b508cfa59de0a54e6eabbfbec069cbb0e57e3
Noah Brahim <noah.alhandy@gmail.com>
2020-10-29 12:56:50 -0700

    Simple Hello World

---
+++ test-1.go
@@ -0,0 +1,14 @@
+//Package Labeled as main
+package main
+
+
+// Importing FMT to Functions
+import "fmt"
+
+//Function Action Say: Hello World
+func main() {
+	fmt.Println("Hello world")
+	
+	// Prints Again
+	fmt.Println("I am trying another thing!")
+}
